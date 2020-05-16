2a97a676fa11198b95e69e0f3c6d8ba50ae7af17
Laith H. Helwany <38575284+laithhelwany@users.noreply.github.com>
2020-05-16 17:26:41 +0300

    Add new comments

--- app.py
+++ app.py
@@ -5,10 +5,14 @@ from flask_cors import CORS
 from models import setup_db, Actor, Movie, association_table, db
 from auth.auth import AuthError, requires_auth
 
+'''
+    return True if one of it's arguments is None
+'''
 
-def is_none(*request_body):
-    for attribute in request_body:
-        if attribute is None:
+
+def is_none(*args):
+    for arg in args:
+        if arg is None:
             return True
     return False
 
@@ -18,27 +22,36 @@ def create_app(test_config=None):
     app = Flask(__name__)
     setup_db(app)
     CORS(app, resources={r"/*": {"origins": "*"}})
-
+    '''
+    Set Access-Control-Allow Headers
+    '''
     @app.after_request
     def after_request(response):
-        # Set Access-Control-Allow Headers
         response.headers.add('Access-Control-Allow-Headers',
                              'Content-Type, Authorization')
         response.headers.add('Access-Control-Allow-Method',
                              'GET, POST, DELETE, PATCH')
         return response
-    # ----Actors-----
 
+
+# ---- Actor Endpoints ----
+
+    '''
+        route handler to get list of actors
+    '''
     @app.route('/actors', methods=['GET'])
     @requires_auth('get:actors')
     def get_actors(token):
         try:
-            actors = Actor.query.all()
+            actors = Actor.query.all()  # Get all actors in the database
             data = []
 
             for actor in actors:
-                data.append({"actor's_informaion": actor.format(),
-                             "actor's_movies": actor.get_movies()})
+                # format actor information
+                data.append({
+                    "actor's_information": actor.format(),
+                    "actor's_movies": actor.get_movies()
+                })
 
             return jsonify({
                 'success': True,
@@ -46,85 +59,138 @@ def create_app(test_config=None):
             })
 
         except Exception:
-            abort(422)
+            abort(422)  # unprocessable
+
+    '''
+        route handler to get actor by id
+    '''
 
     @app.route('/actors/<int:id>', methods=['GET'])
     @requires_auth('get:actors')
     def get_actor(token, id=id):
+        # Retrive the actor from the database
         actor = Actor.query.filter(Actor.id == id).one_or_none()
-        if is_none(actor):
-            abort(404)
+
+        if is_none(actor):  # Check if the actor is not exist
+            abort(404)  # not found
+
         return jsonify({
             'success': True,
-            "actor's_informaion": actor.format(),
+            "actor's_information": actor.format(),
             "actor's_movies": actor.get_movies()
         })
-
+    '''
+        route handler to delete actor by id
+    '''
     @app.route('/actors/<int:id>', methods=['DELETE'])
     @requires_auth('delete:actor')
     def delete_actor(token, id=id):
+        # Retrive the actor from the database
         actor = Actor.query.filter(Actor.id == id).one_or_none()
-        if is_none(actor):
-            abort(404)
+
+        if is_none(actor):  # Check if the actor is not exist
+            abort(404)  # not found
+
         try:
             actor.delete()
             return jsonify({
                 'success': True,
                 'deleted': id
             })
-        except Exception:
-            abort(422)
 
+        except Exception:
+            abort(422)  # unprocessable
+    '''
+        route handler to add new actor
+    '''
     @app.route('/actors', methods=['POST'])
     @requires_auth('add:actor')
     def add_actor(token):
         body = request.get_json()
-        if is_none(body.get('name'), body.get('age'), body.get('gender')):
-            abort(400)
+
+        '''
+            Check if all information are included in the request
+        '''
+        if is_none(
+                body.get('name'),
+                body.get('age'),
+                body.get('gender')):
+
+            abort(404)  # bad request
+
         try:
-            new_actor = Actor(name=body.get('name'), age=body.get(
-                'age'), gender=body.get('gender'))
+            '''
+                Create new Actor
+            '''
+            new_actor = Actor(
+                name=body.get('name'),
+                age=body.get('age'),
+                gender=body.get('gender'))
+
             new_actor.insert()
+
             return jsonify({
                 'success': True,
                 'actor': new_actor.format()
             })
-        except Exception:
-            abort(422)
 
+        except Exception:
+            abort(422)  # unprocessable
+    '''
+        route handler to edit actor information
+    '''
     @app.route('/actors/<int:id>', methods=['PATCH'])
     @requires_auth('edit:actor')
     def edit_actor_data(token, id=id):
+        # Retrive the actor from the database
         actor = Actor.query.filter(Actor.id == id).one_or_none()
-        if actor is None:
-            abort(404)
+
+        if is_none(actor):  # Check if the actor is not exist
+            abort(404)  # not found
+
         try:
+
             body = request.get_json()
-            if body.get('name') is not None:
+            '''
+                Check what information are included in the request body,
+                and edit the actor based on it
+            '''
+            if not is_none(body.get('name')):
                 actor.name = body.get('name')
-            if body.get('age') is not None:
+
+            if not is_none(body.get('age')):
                 actor.age = body.get('age')
-            if body.get('gender') is not None:
+
+            if not is_none(body.get('gender')):
                 actor.gender = body.get('gender')
+
             actor.update()
+
             return jsonify({
                 'success': True,
                 'actor': actor.format()
             })
         except Exception:
-            abort(422)
+            abort(422)  # unprocessable
 
-    # ----- Movies -----
+    # ----- Movies Endpoints -----
 
+    '''
+        route handler to get list of movies
+    '''
     @app.route('/movies', methods=['GET'])
     @requires_auth('get:movies')
     def get_movies(token):
         try:
-            movies = Movie.query.all()
+            movies = Movie.query.all()  # Get all movies in the database
             data = []
 
             for movie in movies:
-                data.append(movie.format())
+                # format movie information
+                data.append({
+                    "movie's_information": movie.format(),
+                    "movie's_movies": movie.get_actors()
+                })
 
             return jsonify({
                 'success': True,
@@ -132,44 +198,75 @@ def create_app(test_config=None):
             })
 
         except Exception:
-            abort(422)
-
+            abort(422)  # unprocessable
+    '''
+        route handler to get movie by id
+    '''
     @app.route('/movies/<int:id>', methods=['GET'])
     @requires_auth('get:movies')
     def get_movie(token, id=id):
+        # Retrive the movie from the database
         movie = Movie.query.filter(Movie.id == id).one_or_none()
-        if is_none(movie):
-            abort(404)
+
+        if is_none(movie):  # Check if the movie is not exist
+            abort(404)  # not found
+
         return jsonify({
             'success': True,
-            'movie_informations': movie.format(),
+            'movie_information': movie.format(),
             "movie's_actors": movie.get_actors()
         })
 
+    '''
+        route handler to delete movie by id
+    '''
     @app.route('/movies/<int:id>', methods=['DELETE'])
     @requires_auth('delete:movie')
     def delete_movie(token, id=id):
+        # Retrive the movie from the database
         movie = Movie.query.filter(Movie.id == id).one_or_none()
-        if is_none(movie):
-            abort(404)
+
+        if is_none(movie):  # Check if the movie is not exist
+            abort(404)  # not found
+
         try:
+
             movie.delete()
+
             return jsonify({
                 'success': True,
                 'deleted': id
             })
-        except Exception:
-            abort(422)
 
+        except Exception:
+            abort(422)  # unprocessable
+
+    '''
+        route handler to add new movie
+    '''
     @app.route('/movies', methods=['POST'])
     @requires_auth('add:movie')
     def add_movie(token):
         body = request.get_json()
-        if is_none(body.get('title'), body.get('release_date')):
-            abort(400)  # bad request
+
+        '''
+            Check if all information are included in the request
+        '''
+
+        if is_none(
+                body.get('title'),
+                body.get('release_date')
+        ):
+            abort(404)  # bad request  # bad request
         try:
-            new_movie = Movie(title=body['title'],
-                              release_date=body['release_date'])
+            '''
+                Create new Movie
+            '''
+            new_movie = Movie(
+                title=body['title'],
+                release_date=body['release_date']
+            )
+
             new_movie.insert()
 
             return jsonify({
@@ -177,70 +274,132 @@ def create_app(test_config=None):
                 'movie': new_movie.format(),
                 "movie's_actors": new_movie.get_actors()
             })
+
         except Exception:
-            abort(422)
+            abort(422)  # unprocessable
+
+    '''
+        route handler to edit movie information
+    '''
 
     @app.route('/movies/<int:id>', methods=['PATCH'])
     @requires_auth('edit:movie')
     def edit_movie_data(token, id=id):
+        # Retrive the movie from the database
         movie = Movie.query.filter(Movie.id == id).one_or_none()
-        if is_none(movie):
-            abort(404)
+
+        if is_none(movie):  # Check if the movie is not exist
+            abort(404)  # not found
         try:
+
             body = request.get_json()
+            '''
+                Check what information are included in the request body,
+                and edit the movie based on it
+            '''
             if not is_none(body.get('title')):
                 movie.title = body.get('title')
+
             if not is_none(body.get('release_date')):
                 movie.release_date = body.get('release_date')
+
             movie.update()
             return jsonify({
                 'success': True,
                 'movie': movie.format()
             })
         except Exception:
-            abort(422)
+            abort(422)  # unprocessable
 
+    # ---- Assign ----
+    '''
+        Assign Endpoint add relationship between actor and movie
+    '''
     @app.route('/assign', methods=['POST'])
     @requires_auth('edit:movie')
     def assign_actor(token):
         body = request.get_json()
-        if is_none(body.get('actor_id'), body.get('movie_id')):
-            abort(400)
+
+        '''
+            Check if all information are included in the request
+        '''
+
+        if is_none(
+            body.get('actor_id'),
+            body.get('movie_id')
+        ):
+            abort(404)  # bad request
+
         actor_id = body.get('actor_id')
         movie_id = body.get('movie_id')
+
+        '''
+        Retrive the Movie and The Actor from the database
+        '''
+
         actor = Actor.query.filter(Actor.id == actor_id).one_or_none()
         movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
-        if is_none(actor, movie):
-            abort(404)
+
+        if is_none(actor, movie):  # Check if any of them is not exist
+            abort(404)  # not found
+
         try:
+
             movie.add_actor(actor)
+
             return jsonify({
                 'success': True,
                 "movie's_actor": movie.get_actors()
             })
+
         except Exception:
-            abort(422)
+            abort(422)  # unprocessable
+
+    '''
+        Assign:Delete Endpoint remove relationship between actor and movie
+    '''
 
     @app.route('/assign', methods=['DELETE'])
     @requires_auth('edit:movie')
     def unassign_actor(token):
         body = request.get_json()
-        if is_none(body.get('actor_id'), body.get('movie_id')):
-            abort(400)
+
+        '''
+            Check if all information are included in the request
+        '''
+
+        if is_none(
+            body.get('actor_id'),
+            body.get('movie_id')
+        ):
+            abort(404)  # bad request
+
         actor_id = body.get('actor_id')
         movie_id = body.get('movie_id')
+
+        '''
+        Retrive the Movie and The Actor from the database
+        '''
+
         actor = Actor.query.filter(Actor.id == actor_id).one_or_none()
         movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
-        if is_none(actor, movie):
+
+        if is_none(actor, movie):  # Check if any of them is not exist
             abort(404)
+
         try:
+
             movie.delete_actor(actor)
+
             return jsonify({
                 'success': True,
                 "movie's_actor": movie.get_actors()
             })
+            
         except Exception:
-            abort(422)
+            abort(422)  # unprocessable
+
+    # ---- Error Handlers ----
 
     @app.errorhandler(422)
     def unprocessable(error):
--- models.py
+++ models.py
@@ -11,6 +11,10 @@ database_path = os.environ['DATABASE_URL']
 
 db = SQLAlchemy()
 
+'''
+    binds a flask application and a SQLAlchemy service
+'''
+
 
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
@@ -19,6 +23,9 @@ def setup_db(app, database_path=database_path):
     db.init_app(app)
 
 
+'''
+    Association Table for actor and movie (Many to Many) relationship
+'''
 association_table = db.Table('association',
                              db.Column('movie_id', db.Integer,
                                        db.ForeignKey('movies.id')),
@@ -28,48 +35,65 @@ association_table = db.Table('association',
 
 
 class Movie(db.Model):
+    # Table name in the database
     __tablename__ = 'movies'
+    # Autoincrementing, unique primary ke
     id = db.Column(db.Integer, primary_key=True)
+
+    # String Title
     title = db.Column(db.String(250), nullable=False)
+
+    # release date
     release_date = db.Column(db.Date(), nullable=False)
+
+    # Create relationship between movie and actor
     actors = db.relationship(
         'Actor', secondary=association_table, backref=db.backref('movies', cascade="all, delete", lazy='joined'))
 
+    # Movie Constructor
     def __init__(self, title, release_date):
         self.title = title
         self.release_date = release_date
 
+    # Insert new Movie into database
     def insert(self):
         db.session.add(self)
         db.session.commit()
 
+    # Add new relationship between movie and actor
     def add_actor(self, actor):
         if actor not in self.actors:
             self.actors.append(actor)
         self.update()
 
+    # Delete the relationship between movie and actor
     def delete_actor(self, actor):
         if actor in self.actors:
             self.actors.remove(actor)
         self.update()
 
+    # Get list movie's actors
     def get_actors(self):
         actors_list = []
+
         for actor in self.actors:
             actors_list.append(actor.format())
+
         return({
             'actors': actors_list
         })
+    # Update Movie
 
     def update(self):
         db.session.commit()
 
+    # Delete the movie from the database
     def delete(self):
         db.session.delete(self)
         db.session.commit()
 
+    # Return Movie information in JSON format 
     def format(self):
-
         return {
             'id': self.id,
             'title': self.title,
@@ -81,46 +105,64 @@ class Movie(db.Model):
 
 
 class Actor(db.Model):
+    # Table Name in the Database
     __tablename__ = 'actors'
+
+    # Autoincrementing, unique primary ke
     id = db.Column(db.Integer, primary_key=True)
+
+    # Name Strign
     name = db.Column(db.String(250), nullable=False)
+
+    # Age Integer
     age = db.Column(db.Integer(), nullable=False)
+
+    # Gender String
     gender = db.Column(db.String(), nullable=False)
 
+    # Actor Constructor
     def __init__(self, name, age, gender):
         self.name = name
         self.age = age
         self.gender = gender
 
+    # Add new relationship between movie and actor
     def add_movie(self, movie):
         if movie not in self.movies:
             self.movies.append(movie)
         self.update()
-
+    # Delete the relationship between movie and actor
     def delete_movie(self, movie):
         if movie in self.movies:
             self.movies.remove(movie)
         self.update()
 
+    # Get list actor's movies
     def get_movies(self):
         movies_list = []
+
         for movie in self.movies:
             movies_list.append(movie.format())
+
         return({
             'movies': movies_list
         })
 
+    # Insert the Actor in the database
     def insert(self):
         db.session.add(self)
         db.session.commit()
-
+    
+    # Update Actor
     def update(self):
         db.session.commit()
 
+    # Delete Actor from the database
     def delete(self):
         db.session.delete(self)
         db.session.commit()
 
+    # Return Actor information in JSON format 
     def format(self):
         return {
             'id': self.id,
