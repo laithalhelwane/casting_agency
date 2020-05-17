8d27128615eedd45e41923007fafc2d3bac5f6b3
Laith H. Helwany <38575284+laithhelwany@users.noreply.github.com>
2020-05-17 21:10:06 +0300

    fix jwt fromater

--- jwt formater.py
+++ jwt formater.py
@@ -1,17 +1,18 @@
-def add_escape(str,long):
+def add_escape(str, long):
     ret = []
     for i in range(len(str)):
         ret.append(str[i])
-        if (1+i) % 50 == 0:
+        if (1+i) % long == 0:
             ret.append("\\\n")
     return "".join(ret)
+
 
 '''
     script for split jwt to multi lines
 '''
 if __name__ == '__main__':
-    long = input("Enter line long")
-    str = input("Enter the JWT")
-    print("Output\n\n")
+    long = int(input("Enter line long: "))
+    str = input("Enter the JWT: ")
+    print("\n\nOutput:\n\n")
     if long != 0:
-        print(add_escape(str,50))
+        print(add_escape(str, long))
