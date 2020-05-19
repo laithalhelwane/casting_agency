2bf8e6276ba63e08857dbb468df992b2267c7ddd
Laith H. Helwany <38575284+laithhelwany@users.noreply.github.com>
2020-05-19 17:31:40 +0300

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-export DATABASE_URL=postgresql://postgres:admin@localhost:5432/app
-export DATABASE_URL_TEST=postgresql://postgres:admin@localhost:5432/app_test
-export AUTH0_DOMAIN=fsnd.au.auth0.com
-export API_AUDIENCE=http://capstone-omc.herokuapp.com/
-export AUTH0_CLIENT_ID=nvEYohEddrdyN3IX0LLyP33S0St7FUc2
-export JWT_SECRET=UpKAk3ooGbY_gJuBbh_ApE9767iVvUfkD2BXX2NM1Re9aYjjesYejnPoY-fEwaCq
+export DATABASE_URL=postgresql://postgres:admin@localhost:5432/app;
+export DATABASE_URL_TEST=postgresql://postgres:admin@localhost:5432/app_test;
+export AUTH0_DOMAIN=fsnd.au.auth0.com;
+export API_AUDIENCE=http://capstone-omc.herokuapp.com/;
+export AUTH0_CLIENT_ID=nvEYohEddrdyN3IX0LLyP33S0St7FUc2;
+export JWT_SECRET=UpKAk3ooGbY_gJuBbh_ApE9767iVvUfkD2BXX2NM1Re9aYjjesYejnPoY-fEwaCq;
